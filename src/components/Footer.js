22643bbf9328f3122d4dd22fa7612be20d43faaa
Corey McCue <coreysmccue@gmail.com>
2021-05-11 20:49:53 +0200

    finish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; <span>Koalafied</span>.  
+        &copy; { new Date().getFullYear() } <span>Koalafied</span>.  
         Built with {""}
         <a href="https://www.gatsbyjs.com">Gatsby</a> by Corey McCue
       </p>
